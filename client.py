009639beb134ef1b6344ec52c692f88f665d9e80
Eric Kimball <eric-k93@live.com>
2017-10-27 17:43:13 -0700

    finished step 7

--- client.py
+++ client.py
@@ -1,19 +1,57 @@
 import sys
 import socket
 
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-    packet = bytearray(2)
+debug = False
 
-    value = int(sys.argv[2])
-    value = value & 0x000000ff
-    packet[0] = value
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+    packet = bytearray(3)
+
+    for i in range(0, len(sys.argv)):
+        print("Printing arg #%d :" % (i), end=' ')
+        print(sys.argv[i])
+
+    print(" --- Entering if statements ---")
+    if sys.argv[2] == "+":
+        operator = 4
+        operator = operator & 0x000000ff
+        print("Operator is \" %s \"" % (sys.argv[2]))
+    elif sys.argv[2] == "-":
+        operator = 2
+        operator = operator & 0x000000ff
+        print("Operator is \" %s \"" % (sys.argv[2]))
+    elif sys.argv[2] == "*":
+        operator = 1
+        operator = operator & 0x000000ff
+        print("Operator is \" %s \"" % (sys.argv[2]))
+
+    packet[0] = operator
+    print(" --- Beginning masking --- ")
     value = int(sys.argv[3])
     value = value & 0x000000ff
+    mask = bin(value)
+    print("argv (3) value is the binary # of " + mask + " after mask...")
     packet[1] = value
-    s.sendto(packet, ("localhost", int(sys.argv[1])))
-    packet = bytearray(4)
-    n = s.recv_into(packet)
 
-    hostInteger = int.from_bytes(packet, byteorder="little", signed=True)
+    value = int(sys.argv[4])
+    value = value & 0x000000ff
+    mask = bin(value)
+    print("argv (4) value is the binary # of " + mask + " after mask...")
+    packet[2] = value
 
-    print("hostInteger: %d" % hostInteger)
+    print(" --- Packet array and assigned values --- ")
+    for i in range(0, len(packet)):
+        print("INDEX: %d VALUE:" % (i), end=' ')
+        print(packet[i])
+
+
+    #send to server
+    if debug != True:
+        s.sendto(packet, ("localhost", int(sys.argv[1])))
+
+        #receive from server
+        packet = bytearray(4)
+        n = s.recv_into(packet)
+
+        hostInteger = int.from_bytes(packet, byteorder="little", signed=True)
+
+        print("hostInteger: %d" % hostInteger)
--- server.py
+++ server.py
@@ -1,9 +1,17 @@
 import sys
 import socket
+
+#start listening - initialize with listening on port argv[1]
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(("", int(sys.argv[1])))
-packet, address = s.recvfrom(2)
-a = packet[0]
+packet, address = s.recvfrom(3)
+
+for i in range(0, len(packet)):
+    print("Packet index %d is: " % (i), end=' ')
+    print(packet[i])
+
+
+a = packet[1]
 print("a was received as: %d" % a)
 
 bit7 = 2**7
@@ -13,7 +21,7 @@ if test_a == bit7:
     a = a - 2**8
 print("a is: %d" % a)
 
-b = packet[1]
+b = packet[2]
 print("b was received as: %d" % b)
 
 test_b = b & bit7
@@ -21,7 +29,17 @@ if test_b == bit7:
     #negative number
     b = b - 2**8
 print("b is: %d" % b)
-hostInteger = a + b
+
+if packet[0] == 4:
+    hostInteger = a + b
+    print("operator received was \"+\"")
+elif packet[0] == 2:
+    hostInteger = a - b
+    print("operator received was \"-\"")
+elif packet[0] == 1:
+    hostInteger = a * b
+    print("operator received was \"*\"")
+
 packet = hostInteger.to_bytes(4, byteorder="little", signed=True)
 
 s.sendto(packet, address)

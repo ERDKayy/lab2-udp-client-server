d6d7684660b5ef81c4216b1c59e4e354cbcdad9b
Eric Kimball <Eric-k93@live.com>
2017-10-31 18:47:41 -0700

    finished task-9, lab is done!

--- client.py
+++ client.py
@@ -5,22 +5,46 @@ debug = False
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     packet = bytearray()
+    intCount = 0
+    for i in range(3, len(sys.argv)):
+        intCount += 1
+
 
     if sys.argv[2] == "+":
         operator = 1
         operator = operator & 0x000000ff
+        operator = operator << 4
     elif sys.argv[2] == "-":
         operator = 2
         operator = operator & 0x000000ff
+        operator = operator << 4
     elif sys.argv[2] == "*":
         operator = 4
         operator = operator & 0x000000ff
-    packet.append(operator)
+        operator = operator << 4
+    packet1 = operator | intCount
+    packet.append(packet1)
 
-    for i in range(3, len(sys.argv)):
-        value = int(sys.argv[i])
-        value = value & 0x000000ff
-        packet.append(value)
+
+    if (intCount % 2 == 0):
+        for i in range(3, len(sys.argv), 2):
+            value = int(sys.argv[i])
+            value = value & 0x000000ff
+            value = value << 4
+            value2 = int(sys.argv[i+1])
+            value = value | value2
+            packet.append(value)
+    else:
+        for i in range(3, len(sys.argv), 2):
+            if i == len(sys.argv) - 1:
+                value = int(sys.argv[i])
+                packet.append(value)
+            else:
+                value = int(sys.argv[i])
+                value = value << 4
+                value2 = int(sys.argv[i+1])
+                value = value | value2
+                packet.append(value)
 
 
     #send to server
--- server.py
+++ server.py
@@ -2,35 +2,57 @@ import sys
 import socket
 
 
-hostInteger = int()
+
 
 #start listening - initialize with listening on port argv[1]
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(("", int(sys.argv[1])))
 packet, address = s.recvfrom(15)
-intNum = packet[1]
-sum = packet[2]
 
-if packet[0] == 1:
-    for i in range(3, len(packet)):
-        print("in the + loop")
-        sum += packet[i]
-    hostInteger = sum
-    print("operator received was \"+\"")
 
-elif packet[0] == 2:
-    for i in range(3, len(packet)):
-        print("in the - loop")
-        sum -= packet[i]
-    hostInteger = sum
-    print("operator received was \"-\"")
+hostInteger = int()
+x = packet[0]
 
-elif packet[0] == 4:
-    for i in range(3, len(packet)):
-        print("in the * loop")
-        sum *= packet[i]
-    hostInteger = sum
-    print("operator received was \"*\"")
+operator = x >> 4
+mask = 2**2
+intCount = x & mask
+
+mask = (2**3) - 1
+
+if operator == 1:
+    for i in range(1, len(packet)):
+        lsb = packet[i] >> 4
+        hostInteger = hostInteger + lsb
+        msb = packet[i] & mask
+        hostInteger = hostInteger + msb
+
+elif operator == 2:
+    for i in range(1, len(packet)):
+        if i == 1:
+            hostInteger = packet[i] >> 4
+            msb = packet[i] & mask
+            hostInteger = hostInteger - msb
+        else:
+            lsb = packet[i] >> 4
+            hostInteger = hostInteger - lsb
+            msb = packet[i] & mask
+            hostInteger = hostInteger - msb
+
+elif operator == 4:
+    for i in range(1, len(packet)):
+        if i == 1:
+            hostInteger = packet[i] >> 4
+            msb = packet[i] & mask
+            hostInteger = hostInteger * msb
+        elif i == len(packet) - 1:
+            msb = packet[i] & mask
+            hostInteger = hostInteger * msb
+        else:
+            lsb = packet[i] >> 4
+            hostInteger = hostInteger * lsb
+            msb = packet[i] & mask
+            hostInteger = hostInteger * msb
+
 
 packet = hostInteger.to_bytes(4, byteorder="little", signed=True)
 s.sendto(packet, address)
